cb6d0ee54d4754d7408893135ce219a2a5a5177f
Pacojc <pacojc1999@gmail.com>
2021-11-23 14:06:51 +0100

    Intento de clave referencial

--- database/migrations/2021_11_23_123402_create_comments_table.php
+++ database/migrations/2021_11_23_123402_create_comments_table.php
@@ -15,12 +15,18 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+            $table->Integer('id_user');
+            $table->Integer('id_point');
             $table->date('date');
             $table->unsignedSmallInteger('valoration');
             $table->string('text');
             $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_point')->references('id')->on('pointofinterest');
         });
     }
+
 
     /**
      * Reverse the migrations.
