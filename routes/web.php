3ee697e5f2ff6e9544c5e5364984aa960ffd09ad
Rosca98 <cristiancod98@gmail.com>
2021-11-24 13:13:45 +0100

    Routa categoria

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/pointofinterest','PointofinterestController');
+Route::resource('/categorie','CategorieController');
 
