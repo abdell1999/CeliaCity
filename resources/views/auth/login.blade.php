348421edc28aa7043ed04d84c4b2ba79d187d8d2
Abdellah <abdellahbukadduh99@gmail.com>
2021-11-24 13:33:12 +0100

    Login funcional

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,9 @@
                                     <div class="text-center">
                                         <h4 class="text-dark mb-4">Welcome Back!</h4>
                                     </div>
-                                    <form class="user">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                    <div class="user">
                                         <div class="mb-3"><input class="form-control form-control-user" type="email"
                                                                  id="exampleInputEmail" aria-describedby="emailHelp"
                                                                  placeholder="Enter Email Address..." name="email">
