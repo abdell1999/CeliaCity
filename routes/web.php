c877d8b934172af3acb22c506f7c60ecbf70eead
Rosca98 <cristiancod98@gmail.com>
2021-11-24 13:57:13 +0100

    ignorar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PointofinterestController;
+use App\Http\Controllers\CategorieController;
 
 /*
 |--------------------------------------------------------------------------
