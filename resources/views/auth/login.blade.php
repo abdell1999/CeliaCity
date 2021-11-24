b9b64e3a34d369846563ad69c0f537859ce0183c
Rosca98 <cristiancod98@gmail.com>
2021-11-24 12:41:55 +0100

    Formateando el codigo html5

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<body class="bg-gradient-primary">
+    <body class="bg-gradient-primary">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9 col-lg-12 col-xl-10">
@@ -9,7 +9,8 @@
                     <div class="card-body p-0">
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-flex">
-                                <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/dogs/image3.jpeg&quot;);"></div>
+                                <div class="flex-grow-1 bg-login-image"
+                                     style="background-image: url(&quot;assets/img/dogs/image3.jpeg&quot;);"></div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
@@ -17,18 +18,35 @@
                                         <h4 class="text-dark mb-4">Welcome Back!</h4>
                                     </div>
                                     <form class="user">
-                                        <div class="mb-3"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email"></div>
-                                        <div class="mb-3"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password"></div>
+                                        <div class="mb-3"><input class="form-control form-control-user" type="email"
+                                                                 id="exampleInputEmail" aria-describedby="emailHelp"
+                                                                 placeholder="Enter Email Address..." name="email">
+                                        </div>
+                                        <div class="mb-3"><input class="form-control form-control-user" type="password"
+                                                                 id="exampleInputPassword" placeholder="Password"
+                                                                 name="password"></div>
                                         <div class="mb-3">
                                             <div class="custom-control custom-checkbox small">
-                                                <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label></div>
+                                                <div class="form-check"><input
+                                                        class="form-check-input custom-control-input" type="checkbox"
+                                                        id="formCheck-1"><label
+                                                        class="form-check-label custom-control-label" for="formCheck-1">Remember
+                                                        Me</label></div>
                                             </div>
-                                        </div><button class="btn btn-primary d-block btn-user w-100" type="submit">Login</button>
-                                        <hr><a class="btn btn-primary d-block btn-google btn-user w-100 mb-2" role="button"><i class="fab fa-google"></i>&nbsp; Login with Google</a><a class="btn btn-primary d-block btn-facebook btn-user w-100" role="button"><i class="fab fa-facebook-f"></i>&nbsp; Login with Facebook</a>
+                                        </div>
+                                        <button class="btn btn-primary d-block btn-user w-100" type="submit">Login
+                                        </button>
+                                        <hr>
+                                        <a class="btn btn-primary d-block btn-google btn-user w-100 mb-2" role="button"><i
+                                                class="fab fa-google"></i>&nbsp; Login with Google</a><a
+                                            class="btn btn-primary d-block btn-facebook btn-user w-100" role="button"><i
+                                                class="fab fa-facebook-f"></i>&nbsp; Login with Facebook</a>
                                         <hr>
                                     </form>
-                                    <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</a></div>
-                                    <div class="text-center"><a class="small" href="register.html">Create an Account!</a></div>
+                                    <div class="text-center"><a class="small" href="forgot-password.html">Forgot
+                                            Password?</a></div>
+                                    <div class="text-center"><a class="small" href="register.html">Create an
+                                            Account!</a></div>
                                 </div>
                             </div>
                         </div>
@@ -38,5 +56,5 @@
         </div>
     </div>
 
-</body>
+    </body>
 @endsection
