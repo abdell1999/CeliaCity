5378f482c41c847c83df9905e8e8cb1d5df1eca9
Pacojc <pacojc1999@gmail.com>
2021-11-24 13:12:33 +0100

    ruta prueba

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/pointofinterest','PointofinterestController');
+
