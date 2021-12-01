843bd6595bc410cfc3b422d680f2695073085096
Rosca98 <cristiancod98@gmail.com>
2021-12-01 13:08:49 +0100

    Solucionando errores vista layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 @include('includes.head')
+<header>
+    @include('includes.header')
+<header>
 </head>
 <body>
-@include('includes.header')
-
-<div class="container">
-    @yield('content')
-</div>
+<div class="container w-full mx-auto pt-20">
+    <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+        @yield('content')
+    </div>
+<div>
 </body>
 </html>
