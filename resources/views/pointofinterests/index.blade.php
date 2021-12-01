609f6cfbe62d35eac95b11f10d62057cc3529f56
Pacojc <pacojc1999@gmail.com>
2021-12-01 13:47:10 +0100

    Vista bonica de puntos de interes lista

--- resources/views/pointofinterests/index.blade.php
+++ resources/views/pointofinterests/index.blade.php
@@ -1,55 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Punto de interes</h1>
+<section class="bg-blueGray-50">
+    <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-10">
+        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+            <div class="rounded-t mb-0 px-4 py-3 border-0">
+                <div class="flex flex-wrap items-center">
+                    <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                        <h3 class="font-semibold text-base text-blueGray-700">Puntos de interés</h3>
+                    </div>
+                    <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                        <a href="{{ route('pointofinterests.create') }}"
+                            class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            type="button">Puntos de interés</a>
+                    </div>
+                </div>
+            </div>
 
-<a href=" {{ route('pointofinterests.create') }} ">Nuevo punto de interes</a>
+            <div class="block w-full overflow-x-auto">
+                <table class="items-center bg-transparent w-full border-collapse ">
+                    <thead>
+                        <tr>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Nombre
+                            </th>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Ubicación
+                            </th>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Número movil
+                            </th>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Texto
+                            </th>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Acciones
+                            </th>
+                        </tr>
+                    </thead>
 
-<div class="col-md10 mx-auto bg-white p-3">
-    <table class="table">
-        <thead>
-            <tr>
-                <th scole="col">Nombre</th>
-                <th scole="col">Ubicación</th>
-                <th scole="col">movilephone</th>
-                <th scole="col">text</th>
-            </tr>
-        </thead>
+                    <tbody>
+                        @foreach ($pointofinterests as $pointofinterest)
+                        <tr>
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                {{$pointofinterest->name}}
+                            </th>
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                {{$pointofinterest->ubication}}
+                            </th>
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                {{$pointofinterest->movilephone}}
+                            </th>
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                {{$pointofinterest->text}}
+                            </th>
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                <form method="POST" action="{{ route('pointofinterests.destroy', $pointofinterest->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" class="btn btn-danger w-20 d-block" value="Eliminar">
+                                </form>
+                                <a href="{{ route('pointofinterests.edit', $pointofinterest->id) }}"
+                                    class="btn btn-dark mr-1">Editar</a>
+                                <br>
+                                <a href="{{ route('pointofinterests.show', $pointofinterest->id) }}"
+                                    class="btn btn-success mr-1">Ver</a>
+                            </th>
+                            @endforeach
+                    </tbody>
 
+                </table>
+            </div>
+        </div>
+    </div>
 
-        <tbody>
-            @foreach ($pointofinterests as $pointofinterest)
-            <tr>
-                <td> {{$pointofinterest->name}} </td>
-                <td> {{$pointofinterest->ubication}} </td>
-                <td> {{$pointofinterest->movilephone}} </td>
-                <td> {{$pointofinterest->text}} </td>
-                <td>
-                   
-
-
-                <form method="POST" action="{{ route('pointofinterests.destroy', $pointofinterest->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" class="btn btn-danger w-20 d-block" value="Eliminar">
-                    </form>
-                    <a href="{{ route('pointofinterests.edit', $pointofinterest->id) }}" class="btn btn-dark mr-1">Editar</a>
-                    <br>
-                    <a href="{{ route('pointofinterests.show', $pointofinterest->id) }}" class="btn btn-success mr-1">Ver</a>
-                </td>
-
-
-
-
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-
-
-    </table>
-
-</div>
-
-
-@endsection
+    @endsection
