8eb70f7da31c9bf65c0325cc7a5bbfd190faa483
Rosca98 <cristiancod98@gmail.com>
2021-12-01 13:34:17 +0100

    Arregalada vista categorias

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,30 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <div>
-        <h1>Categorais</h1>
-        <a href=" {{ route('categories.create') }} ">Nueva Categoria</a>
-        <div>
+<section class="bg-blueGray-50">
+    <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-10">
+        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+            <div class="rounded-t mb-0 px-4 py-3 border-0">
+                <div class="flex flex-wrap items-center">
+                    <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                        <h3 class="font-semibold text-base text-blueGray-700">Categorias</h3>
+                    </div>
+                    <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                        <a href="{{ route('categories.create') }}"
+                            class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            type="button">Añadir Categoria</a>
+                    </div>
+                </div>
+            </div>
 
-            <div class="p-5">
-                <table class="w-full p-5 text-gray-700">
+            <div class="block w-full overflow-x-auto">
+                <table class="items-center bg-transparent w-full border-collapse ">
                     <thead>
                         <tr>
-                            <th class="text-left text-gray-600">Nombre</th>
-                            <th class="text-left text-gray-600">Acciones</th>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Nombre
+                            </th>
+                            <th
+                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Acciones
+                            </th>
                         </tr>
                     </thead>
-
 
                     <tbody>
                         @foreach ($categories as $categorie)
                         <tr>
-                            <td> {{$categorie->name}} </td>
-                            <td>
-
-
-
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                {{$categorie->name}}
+                            </th>
+                            <th
+                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
                                 <form method="POST" action="{{ route('categories.destroy', $categorie->id) }}">
                                     @csrf
                                     @method('DELETE')
@@ -35,23 +51,13 @@
                                 <br>
                                 <a href="{{ route('categories.show', $categorie->id) }}"
                                     class="btn btn-success mr-1">Ver</a>
-                            </td>
-
-
-
-
-                            </td>
-                        </tr>
-                        @endforeach
+                            </th>
+                            @endforeach
                     </tbody>
 
-
                 </table>
-
             </div>
         </div>
     </div>
-</div>
-</div>
 
-@endsection
+    @endsection
