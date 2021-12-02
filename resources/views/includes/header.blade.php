8adb2ebfa063049cfa0bbbd8d08d4198740ea2b9
Rosca98 <cristiancod98@gmail.com>
2021-12-02 12:10:18 +0100

    Añadiendo ruta de usuarios

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -64,7 +64,7 @@
                     </a>
                 </li>
                 <li class="mr-6 my-2 md:my-0">
-                    <a href="#"
+                    <a href="{{ route('users.index') }}"
                         class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-100 border-b-2 border-gray-900  hover:border-pink-400">
                         <i class="fas fa-users fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Usuarios</span>
                     </a>
