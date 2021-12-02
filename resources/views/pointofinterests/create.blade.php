31462a89296bbd21d7965221a3cef415149c5c29
Burlof <jose_david_51@hotmail.com>
2021-12-02 20:20:17 +0100

    Nuevo estilo para la vista editar de puntos de interés

--- resources/views/pointofinterests/create.blade.php
+++ resources/views/pointofinterests/create.blade.php
@@ -56,7 +56,6 @@
         </div>
 
 <div class="md:w-1/3">
-
     <button
         class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
         Enviar
--- resources/views/pointofinterests/edit.blade.php
+++ resources/views/pointofinterests/edit.blade.php
@@ -5,11 +5,69 @@
 <form action="{{ route('pointofinterests.update',['pointofinterest' => $pointofinterests->id]) }}" enctype="multipart/form-data" method = "post">
     @csrf
     @method('put')
-                    Nombre:<input type="text" name="name"required value="{{ $pointofinterests->name }}" required><br>
+                    Nombre:<input type="text" name="name" value="{{ $pointofinterests->name }}" required><br>
                     Ubicación:<input type="text" name="ubication" value="{{ $pointofinterests->ubication }}" required><br>
                     Número móvil:<input type="text" name="movilephone" value="{{ $pointofinterests->movilephone }}" required><br>
                    Contenido:<input type="text" name="text" value="{{ $pointofinterests->text }}" required><br>
 
-<input type="submit" value="enviar">
+
+        <div class="w-full max-w-sm">
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
+                        Nombre
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-full-name" type="text" id="name" placeholder="Nombre de la nueva categoría" name="name" value="{{ $pointofinterests->name }}" required>
+                </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="ubication">
+                        Ubicación
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-full-name" type="text" id="ubication" placeholder="Nombre de la nueva ubicación" name="ubication" value="{{ $pointofinterests->ubication }}" required>
+                </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="movilephone">
+                        Número móvil
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-full-name" type="text" id="movilephone" placeholder="Nombre del nuevo teléfono" name="movilephone" value="{{ $pointofinterests->movilephone }}" required>
+                </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="ubication">
+                        Contenido
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-full-name" type="text" id="text" placeholder="Nombre del nuevo contenido" name="text" value="{{ $pointofinterests->text }}" required>
+                </div>
+            </div>
+        </div>
+
+    <div class="md:w-1/3">
+        <button
+            class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+            Enviar
+        </button>
+    </div>
+</form>
 
 @endsection
