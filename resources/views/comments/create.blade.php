613e462d1fea4e5807bb882b300ac88c09bdfe0b
Jose David <jose_david_51@hotmail.com>
2021-12-03 13:32:15 +0100

    Creación vista CreateComments

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -26,31 +26,43 @@
                 <div class="md:w-2/3">
                     <input
                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        id="inline-full-name" type="text" id="valoration" placeholder="Indique la nueva valoración" name="ubication">
+                        id="inline-full-name" type="text" id="valoration" placeholder="Indique la nueva valoración" name="valoration">
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="movilephone">
-                        Número móvil
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="text">
+                        Comentario
                     </label>
                 </div>
                 <div class="md:w-2/3">
                     <input
                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        id="inline-full-name" type="text" id="movilephone" placeholder="Nombre del nuevo teléfono" name="movilephone">
+                        id="inline-full-name" type="text" id="text" placeholder="Texto del nuevo comentario" name="text">
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="ubication">
-                        Contenido
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="idUser">
+                        id User
                     </label>
                 </div>
                 <div class="md:w-2/3">
                     <input
                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        id="inline-full-name" type="text" id="text" placeholder="Nombre del nuevo contenido" name="text">
+                        id="inline-full-name" type="text" id="idUser" placeholder="id del nuevo usuario" name="idUser">
+                </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="idPoint">
+                        id Point
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-full-name" type="text" id="idPoint" placeholder="id del nuevo punto" name="idPoint">
                 </div>
             </div>
         </div>
