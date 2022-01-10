2514378315ce910c6c92a2f992f7a262f1703a25
Rosca98 <cristiancod98@gmail.com>
2022-01-10 11:57:26 +0100

    Arreglado front en modo movil y añadido la funcion de mostrar todas las categorias de la base de datos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categorie;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data['categories'] = Categorie::all();
+        return view('home', $data);
     }
 }
