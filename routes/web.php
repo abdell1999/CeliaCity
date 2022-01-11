67691ad8ed2de502fcfda815c93e32a712fc8b38
Abdellah <abdellahbukadduh99@gmail.com>
2022-01-11 09:35:07 +0100

    Ruta principal arreglada

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
