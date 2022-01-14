e5dc849037eeae1f4aa06e03c4d3117957ff2654
Abdellah <abdellahbukadduh99@gmail.com>
2022-01-14 12:50:55 +0100

    Confirmación antes de eliminar una categoría casí terminada, en html funciona correctamente en laravel no funciona el boton para mostrar la ventana pero la ventana funciona correctamente si se fuerza su visualización

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,55 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-
-
-
-
-
-
-
-
-
-        <section class="bg-blueGray-50">
-            <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-10">
-                <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
-                    <div class="rounded-t mb-0 px-4 py-3 border-0">
-                        <div class="flex flex-wrap items-center">
-                            <div class="relative w-full px-4 max-w-full flex-grow flex-1">
-                                <h1 class="font-semibold text-base text-xl text-blueGray-700">Categorias</h1>
-                            </div>
-                            <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                                <a href="{{ route('categories.create') }}"
-                                    class="bg-indigo-500 text-white text-sm active:bg-indigo-600 font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    type="button">Añadir Categoria</a>
-                            </div>
+    <div class="bg-blueGray-50">
+        <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-10">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+                <div class="rounded-t mb-0 px-4 py-3 border-0">
+                    <div class="flex flex-wrap items-center">
+                        <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                            <h1 class="font-semibold text-base text-xl text-blueGray-700">Categorias</h1>
+                        </div>
+                        <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                            <a href="{{ route('categories.create') }}"
+                                class="bg-indigo-500 text-white text-sm active:bg-indigo-600 font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button">Añadir Categoria</a>
                         </div>
                     </div>
+                </div>
 
-                    <div class="block w-full overflow-x-auto">
-                        <table class="items-center bg-transparent w-full border-collapse ">
-                            <thead>
-                                <tr>
-                                    <th
-                                        class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold ">
-                                        Nombre
-                                    </th>
-                                    <th
-                                        class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold ">
-                                        Editar
-                                    </th>
-                                    <th
-                                        class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold ">
-                                        Borrar
-                                    </th>
-                                </tr>
-                            </thead>
+                <div class="block w-full overflow-x-auto">
+                    <table class="items-center bg-transparent w-full border-collapse ">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold ">
+                                    Nombre
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold ">
+                                    Editar
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold ">
+                                    Borrar
+                                </th>
+                            </tr>
+                        </thead>
 
-                            <tbody>
-                                @foreach ($categories as $categorie)
+                        <tbody>
+                            @foreach ($categories as $categorie)
 
-                                <body x-data="{showModal{{ $categorie->id }}: false}" :class="{
-                                    'overflow-y-hidden': showModal{{ $categorie->id }}}">
+                                <div x-data="{showModal{{ $categorie->id }}: false}" :class="{'overflow-y-hidden': showModal{{ $categorie->id }}}">
                                     <!-- Modales -->
                                     <div class="fixed inset-0 w-full h-full z-20 bg-black bg-opacity-50 duration-300 overflow-y-auto"
                                         x-show="showModal{{ $categorie->id }}"
@@ -131,13 +121,14 @@
 
                                         </th>
                                     </tr>
-                                @endforeach
-                            </tbody>
+                                </div>
+                            @endforeach
+                        </tbody>
 
-                        </table>
-                    </div>
+                    </table>
                 </div>
             </div>
+        </div>
 
 
 
@@ -147,7 +138,7 @@
 
 
 
-        </body>
+    </div>
 
 
-@endsection
+</ @endsection
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,70 +1,59 @@
 @extends('layouts.front')
 @section('content')
 
-<header>
-           <div class="flex justify-center ">
-                <h1 class="italic text-3xl">Restaurantes de Purullena</h1>
-            </div>
-                      <div class="ml-10 mt-10 md:flex md:flex-row md:justify-evenly">
-                        <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                          <a href="#!">
-                            <div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">
-                                <div class="carousel-inner relative w-full overflow-hidden">
-                                  <div class="carousel-item active relative float-left w-full">
-                                    <img
-                                      src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-                                      class="block w-full"
-                                      alt="Wild Landscape"
-                                    />
-                                  </div>
-                                  <div class="carousel-item relative float-left w-full">
-                                    <img
-                                      src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-                                      class="block w-full"
-                                      alt="Camera"
-                                    />
-                                  </div>
-                                  <div class="carousel-item relative float-left w-full">
-                                    <img
-                                      src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-                                      class="block w-full"
-                                      alt="Exotic Fruits"
-                                    />
-                                  </div>
-                                </div>
-                                <button
-                                  class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-                                  type="button"
-                                  data-bs-target="#carouselExampleControls"
-                                  data-bs-slide="prev"
-                                >
-                                  <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                                  <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button
-                                  class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-                                  type="button"
-                                  data-bs-target="#carouselExampleControls"
-                                  data-bs-slide="next"
-                                >
-                                  <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                                  <span class="visually-hidden">Next</span>
-                                </button>
-                              </div>
-                          </a>
-                          <div class="p-6">
-                            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-                            <p class="text-gray-700 text-base mb-4">
-                              Some quick example text to build on the card title and make up the bulk of the card's
-                              content.
-                            </p>
-                            
-                          </div>
+    <header>
+        <div class="flex justify-center ">
+            <h1 class="italic text-3xl">{{ $categorie->name }}</h1>
+        </div>
+
+
+        <div class="ml-10 mt-10 md:flex md:flex-row md:justify-evenly flex-wrap">
+
+            @foreach ($selected as $seleccionado )
+
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!">
+                    <div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">
+                        <div class="carousel-inner relative w-full overflow-hidden">
+                            <div class="carousel-item active relative float-left w-full">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" class="block w-full"
+                                    alt="Wild Landscape" />
+                            </div>
+                            <div class="carousel-item relative float-left w-full">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" class="block w-full"
+                                    alt="Camera" />
+                            </div>
+                            <div class="carousel-item relative float-left w-full">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" class="block w-full"
+                                    alt="Exotic Fruits" />
+                            </div>
                         </div>
-                        
+                        <button
+                            class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                            type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button
+                            class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                            type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </a>
+
+                <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">{{$seleccionado->name}}</h5>
+                    <p class="text-gray-700 text-base mb-4">{{$seleccionado->text}}</p>
+
+                </div>
+            </div>
+
+            @endforeach
 
             </div>
-            
-        </header>
+
+    </header>
 
 @endsection
