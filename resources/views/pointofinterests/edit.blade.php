c55c06d3d0c3c34d8dbd179b0d5164128cf86293
Rosca98 <cristiancod98@gmail.com>
2022-01-17 09:22:01 +0100

    Arreglado la parte de puntos de interes a la hora de editar para eliminar o añadir categorias

--- resources/views/pointofinterests/edit.blade.php
+++ resources/views/pointofinterests/edit.blade.php
@@ -67,6 +67,27 @@
                         </textarea>
                 </div>
             </div>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="categorie">
+                        Categorias a la que pertenece
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                <span class="text-gray-700">Selección multiple</span>
+                    <select class="form-multiselect block w-full mt-1 bg-gray-200 appearance-none border-2 border-gray-200 rounded text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="categoriespoint[]" multiple="multiple">
+                        @foreach($categories as $categorie)
+                        <option value="{{$categorie['id']}}"
+                        @foreach ($categoriespoint as $categoriepoint)
+                            @if ($categoriepoint->id_categorie == $categorie->id)
+                                selected
+                            @endif
+                        @endforeach
+                        >{{$categorie['name']}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
         </div>
 
     <div class="md:w-1/3">
