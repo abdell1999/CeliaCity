042ecc1ee666ba486b6ec5facea713b3d7e8f336
Abdellah <abdellahbukadduh99@gmail.com>
2022-01-26 09:28:53 +0100

    El index de usuarios carga la lista de usuarios mediante ajax

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -9,7 +9,49 @@ $(document).ready(function () {
             url: "/fetch-user",
             dataType: "json",
             success: function (response) {
-                console.log("SUCCESS");
+                $('tbody').html("");
+                console.log("Hay respuesta por parte del servidor");
+                $.each(response.user, function (key, item) {
+
+                    rol = "Error";
+                    if(item.rol == 0){
+                        rol = "Administrador";
+                    }
+
+                    if(item.rol == 1){
+                        rol = "Editor";
+
+                    }
+
+                    if(item.rol == 2){
+                        rol = "Usuario";
+
+                    }
+
+
+
+                    $('tbody').append("<tr id='+"+ item.id +"'>\
+                    <td class='px-6 py-4 whitespace-nowrap'>\
+                    <div class='flex items-center'>\
+                    <div class='flex-shrink-0 h-10 w-10'>\
+                    <img class='h-10 w-10 rounded-full' src='"+ item.photo + "' alt='' />\
+                    </div>\
+                    <div class='ml-4'>\
+                    <div class= 'text-sm font-medium text-gray-900' > " + item.name + " " + item.surname1 + " " + item.surname2 + "</div >\
+                    <div class='text-sm text-gray-500'>"+ item.email +"</div>\
+                    </div></div></td><td class='px-6 py-4 whitespace-nowrap'>\
+                    <div class='text-sm text-gray-900'>"+ item.address +"</div>\
+                    <div class='text-sm text-gray-700'>"+ item.phone + " / " + item.movilphone +"</div></td>\
+                    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>"+ item.borndate +"</td>\
+                    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>"+ rol +"</td>\
+                    <td class='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>\
+                    <a href='#' class='text-indigo-600 hover:text-indigo-900'>View</a>\
+                    <a href='#' class='text-indigo-600 hover:text-indigo-900'>Edit</a>\
+                    <a href='#' class='text-indigo-600 hover:text-indigo-900'>Delete</a></td></tr>\
+                    </tr>");
+                })
+
+
                 console.log(response);
             },
             error: function (response) {
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -303,47 +303,7 @@
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach($users as $user)
-                        <tr>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="flex-shrink-0 h-10 w-10">
-                                        <img class="h-10 w-10 rounded-full" src="{{ url($user->photo) }}" alt="" />
-                                    </div>
-                                    <div class="ml-4">
-                                        <div class="text-sm font-medium text-gray-900">
-                                            {{ $user->name . ' ' . $user->surname1 . ' ' . $user->surname2 }}
-                                        </div>
-                                        <div class="text-sm text-gray-500">
-                                            {{ $user->email }}
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <div class="text-sm text-gray-900">{{ $user->address }}</div>
-                                <div class="text-sm text-gray-700">
-                                    {{ $user->movilphone . '/' . $user->phone }}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                {{ $user->borndate }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
 
-                                @if($user->rol == 0) Administrador @endif @if($user->rol == 1) Editor @endif @if($user->rol == 2) Usuario @endif
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a href="{{ route('users.show', $user->id) }}" class="text-indigo-600 hover:text-indigo-900">View</a>
-                                <a href="{{ route('users.edit', $user->id) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-
-                                <form method="POST" action="{{ route('users.destroy', $user->id) }}">
-                                    @csrf @method('DELETE')
-                                    <input type="submit" class="text-indigo-600 hover:text-indigo-900" value="Delete">
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
                     </tbody>
                 </table>
             </div>
