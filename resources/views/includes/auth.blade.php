e3ff4b454b92c0107d9a85e349b56942aff53d4a
Abdellah <abdellahbukadduh99@gmail.com>
2022-02-03 16:54:47 +0100

    Pequeño error de login solucionado

--- resources/views/includes/auth.blade.php
+++ resources/views/includes/auth.blade.php
@@ -283,7 +283,7 @@
                                     <input type="password" class="form-control" name="password" id="password"
                                         placeholder="Contraseña">
                                 </div>
-                                <button type="button" class="btn btn-info btn-block btn-round">Acceder</button>
+                                <input type="submit" class="btn btn-info btn-block btn-round">Acceder</button>
                             </form>
 
 
