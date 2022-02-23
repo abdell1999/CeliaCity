917ba23a94b770ee4c6bacd3a77941b94346b915
Pacojc <pacojc1999@gmail.com>
2022-02-23 14:18:10 +0100

    Ruta home

--- resources/views/includes/headerfront.blade.php
+++ resources/views/includes/headerfront.blade.php
@@ -18,7 +18,7 @@
                         <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                             <ul id="nav" class="navbar-nav ml-auto">
                                 <li class="nav-item">
-                                    <a class="page-scroll" href="#home">Home</a>
+                                    <a class="page-scroll" href="/home">Home</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="page-scroll" href="/guide">Guia</a>
