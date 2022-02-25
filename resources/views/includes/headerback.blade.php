e8a6089f052772142342d1bcd4c8fdf4b6365326
Jose David <jose_david_51@hotmail.com>
2022-02-25 12:33:05 +0100

    Arreglado el enlace home del panel de administración y los colores de los botones

--- resources/views/includes/headerback.blade.php
+++ resources/views/includes/headerback.blade.php
@@ -58,7 +58,7 @@
             id="nav-content">
             <ul class="list-reset lg:flex flex-1 items-center px-4 md:px-0">
                 <li class="mr-6 my-2 md:my-0">
-                    <a href="#"
+                    <a href="{{ route('home') }}"
                         class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-100 border-b-2 border-gray-900  hover:border-pink-400">
                         <i class="fas fa-home fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Home</span>
                     </a>
