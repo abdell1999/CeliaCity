d090c8b0b466ac8eb6390a64e1263973ff6d35a7
Abdellah <abdellahbukadduh99@gmail.com>
2022-02-26 20:17:13 +0100

    Añadido método que trae un punto a las excepciones de auth

--- app/Http/Controllers/PointofinterestController.php
+++ app/Http/Controllers/PointofinterestController.php
@@ -13,7 +13,7 @@ class PointofinterestController extends Controller
 
 
     public function __construct(){
-        $this->middleware("auth")->except("show");
+        $this->middleware("auth")->except("show","fetchonepoint");
     }
 
 
